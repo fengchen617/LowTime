3a3e056216d1207383bd1af1f2dfc544dbbfab04
fengcehn <316925038@qq.com>
2019-12-10 17:09:34 +0800

    Low Time Welcome Ok

---
+++ app/src/main/java/com/bawei6/lowtime/ui/Welcome_Select_Activity.java
@@ -0,0 +1,14 @@
+package com.bawei6.lowtime.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome_Select_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome__select_);
+    }
+}
