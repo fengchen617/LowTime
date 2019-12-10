e4cb9844ef525df707489e2b7cfd03110b2acef5
fengcehn <316925038@qq.com>
2019-12-10 19:07:19 +0800

    Tow Time Welcome Ok

--- app/src/main/java/com/bawei6/lowtime/ui/WelcomeActivity.java
+++ app/src/main/java/com/bawei6/lowtime/ui/WelcomeActivity.java
@@ -1,14 +1,52 @@
 package com.bawei6.lowtime.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.bawei6.lowtime.R;
 
+/**
+ * name:fengchen
+ * time:19.12.10
+ *       欢迎界面
+ */
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView welcome_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initView();
+    }
+
+    private void initView() {
+        welcome_text = (TextView) findViewById(R.id.welcome_text);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.welcome_animation);
+        welcome_text.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(WelcomeActivity.this, Welcome_Select_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/bawei6/lowtime/ui/Welcome_Select_Activity.java
+++ app/src/main/java/com/bawei6/lowtime/ui/Welcome_Select_Activity.java
@@ -1,14 +1,59 @@
 package com.bawei6.lowtime.ui;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.bawei6.lowtime.R;
+import com.bumptech.glide.Glide;
 
-public class Welcome_Select_Activity extends AppCompatActivity {
+/**
+ * name:fengchen
+ * time:19.12.10
+ * 欢迎界面——选择  年   月  日
+ */
+
+public class Welcome_Select_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView welcome_select_img;
+    private Button w_s_b_year;
+    private Button w_s_b_mounth;
+    private Button w_s_b_day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome__select_);
+        initView();
+    }
+
+    private void initView() {
+        welcome_select_img = (ImageView) findViewById(R.id.welcome_select_img);
+        Glide.with(this).load("http://hbimg.b0.upaiyun.com/9852feef26dcba3d807282aa5dff561b1e223de85b027-Tzv0x0_fw658").into(welcome_select_img);
+
+        w_s_b_year = (Button) findViewById(R.id.w_s_b_year);
+        w_s_b_year.setOnClickListener(this);
+        w_s_b_mounth = (Button) findViewById(R.id.w_s_b_mounth);
+        w_s_b_mounth.setOnClickListener(this);
+        w_s_b_day = (Button) findViewById(R.id.w_s_b_day);
+        w_s_b_day.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.w_s_b_year:
+
+                break;
+            case R.id.w_s_b_mounth:
+
+                break;
+            case R.id.w_s_b_day:
+
+                break;
+        }
     }
 }
